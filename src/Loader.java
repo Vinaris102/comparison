1cf51b0abd618e0ee3eaa5b189019bac32f9f3b2
vinaris <vinaris04@mail.ru>
2018-11-23 21:47:54 +0500

    init

---
+++ src/Loader.java
@@ -0,0 +1,16 @@
+
+public class Loader {
+    public static void main(String[] args) {
+        Integer dimaAge = 18;
+        Integer mishaAge = 18;
+
+        if (dimaAge.equals(mishaAge))
+        {
+            System.out.println("Одинаковый возраст");
+        }
+        else
+        {
+            System.out.println("Разный возраст");
+        }
+    }
+}
